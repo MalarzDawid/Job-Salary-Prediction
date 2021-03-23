be1e3bed77d7659577fa8c61173978c73b22b59b
MalarzDawid <github@dawidmalarz.pl>
2021-03-23 15:15:03 +0100

    Load model from pickle

--- projects/justjoin-jobs/app/app.py
+++ projects/justjoin-jobs/app/app.py
@@ -1,3 +1,4 @@
+import pickle
 import streamlit as st
 import pandas as pd
 
@@ -5,6 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
+
+
+with open("models/linear_model", "rb") as file:
+    model = pickle.load(file)
 
 df = pd.read_csv("../data/clear_output_data.csv")
 
@@ -20,7 +25,7 @@ experience_level = df['experience_level'].value_counts().index
 contract_type = df['contract_type'].value_counts().index
 remote_interview = ['True', 'False']
 remote = ['True', 'False']
-company_size = [0, 1, 2]
+company_size = ['very_small', 'small', 'medium', 'large']
 
 
 col1, col2 = st.beta_columns(2)
@@ -63,35 +68,28 @@ technology_input = st.selectbox(
 
 # Contract type
 contract_type_input = st.radio(
-    "Contract Type:",
+    'Contract Type:',
     contract_type
 )
 
 if st.button('Predict!'):
-    y = df['salary_mean']
     X = df.drop('salary_mean', axis=1)
+    
     inputs = [city_input, technology_input, workplace_type_input, experience_level_input, contract_type_input, company_size_input]
+    
     df1 = pd.DataFrame([inputs], columns=X.columns)
     X = X.append(df1)
 
     X['city'] = X['city'].astype('category')
     X['city'] = X['city'].cat.codes
+
     X['marker_icon'] = X['marker_icon'].astype('category')
     X['marker_icon'] = X['marker_icon'].cat.codes
-    X = pd.get_dummies(X, columns=['workplace_type', 'experience_level', 'contract_type'], prefix="feature")
+    X = pd.get_dummies(X, columns=['workplace_type', 'experience_level', 'contract_type', 'company_size_bin'], prefix="feature")
+
     predict_value = X.tail(1)
     X = X[:-1]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2021)
-    linear_model = LinearRegression(normalize=True)
-
-    # Train
-    linear_model.fit(X_train, y_train)
-
-    # Predict
-    prediction_test = linear_model.predict(X_test)
-    prediction_train  = linear_model.predict(X_train)
-
-    predict_ = linear_model.predict(predict_value)[0]
+    predict_ = model.predict(predict_value)[0]
     st.header("Done...")
     st.success(f"Predict value: {predict_}")
