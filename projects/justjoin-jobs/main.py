f57fdad533874067d3eb49d3083b89df3fcae7ec
MalarzDawid <github@dawidmalarz.pl>
2021-03-23 12:53:43 +0100

    Move code from main.py to notebook

--- projects/justjoin-jobs/main.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pandas as pd
-
-request = requests.get("https://justjoin.it/api/offers")
-data = request.json()
-
-df = pd.DataFrame.from_dict(data)
-df.to_csv("justjoin_jobs.csv")
